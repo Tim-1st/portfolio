6a406e86267aef6f1e8f65639275c28c33613a9b
Timéo <timeodc@icloud.com>
2026-01-08 09:36:06 +0100

    Update

--- include/js/photographie.js
+++ include/js/photographie.js
@@ -20,3 +20,16 @@ const burgerMenu = document.querySelector('.burger-menu');
             ancres.classList.remove('active');
         }
     });
+
+
+const scrollBtn = document.getElementById('scrollTopBtn');
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 200) {
+            scrollBtn.classList.add('visible');
+        } else {
+            scrollBtn.classList.remove('visible');
+        }
+});
+scrollBtn.addEventListener('click', function() {
+window.scrollTo({ top: 0, behavior: 'smooth' });
+});
